9a080230760691364caa700f5e952f70fd059927
Xinle Chen <chenxinle3751@gmail.com>
2024-09-27 15:30:22 -0600

    updated Star class

--- app/src/main/java/com/example/vortex/Star.java
+++ app/src/main/java/com/example/vortex/Star.java
@@ -1,8 +1,39 @@
 package com.example.vortex;
 
 public class Star extends Shape{
-    public Star (int x, int y){
-        this.x = x;
-        this.y = y;
+
+    private int points;
+    private double innerRadius;
+    private double outerRadius;
+
+    public Star (int x, int y, int points, double innerRadius, double outerRadius){
+        super(x,y);
+        this.points = points;
+        this.innerRadius = innerRadius;
+        this.outerRadius = outerRadius;
+    }
+
+    //getters and setters
+    public int getPoints(){
+        return points;
+    }
+
+    public void setPoints(int points){
+        this.points = points;
+    }
+
+    public double getInnerRadius(){
+        return innerRadius;
+    }
+
+    public void setInnerRadius(double innerRadius){
+        this.innerRadius = innerRadius;
+    }
+
+    public double getOuterRadius(){
+        return outerRadius;
+    }
+    public void setOuterRadius(double outerRadius){
+        this.outerRadius = outerRadius;
     }
 }
